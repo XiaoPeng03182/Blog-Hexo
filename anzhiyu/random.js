ee734d59fe9f30c302a5ce740f3a6092b49ba80b
XiaoPeng03182 <2872259389@qq.com>
2024-09-07 23:34:18 +0800

    Site updated: 2024-09-07 23:34:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/07/hello-world/"];function toRandomPost(){
+var posts=["2024/09/07/hello-world/","2024/09/02/Linux-Learing/Linux-Learning/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
